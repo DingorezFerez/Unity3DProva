eae0f760a2746205cc02a33fab099d1df1a814a2
DingorezFerez <luca.miranda91@gmail.com>
2017-06-17 18:23:05 +0200

    Add files via upload

---
+++ Assets/AnalyticsScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class AnalyticsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LevelUp();
+	}
+
+    void LevelUp()
+    {
+        Analytics.Transaction("levelUp", 1, "EUR", null, null);
+
+        Analytics.CustomEvent("levelUp", new Dictionary<string, object>
+            {
+                { "newLevel", 2 },
+                { "trigger", "purchase"},
+            });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
